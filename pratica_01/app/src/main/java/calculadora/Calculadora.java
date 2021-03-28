e7455b163f6e456fa7b042dec4695debf31cba3d
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-03-28 19:12:02 -0300

    :adhesive_bandage: Conserta arredondamento metodo raizNesima esp. para numeros inteiros

--- pratica_01/app/src/main/java/calculadora/Calculadora.java
+++ pratica_01/app/src/main/java/calculadora/Calculadora.java
@@ -1,8 +1,18 @@
 package calculadora;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
 public class Calculadora {
+
   public double raizNesima(double radicando, double indice) {
+    return raizNesima(radicando, indice, 8);
+  }
+
+  public double raizNesima(
+          double radicando, double indice, int roundPlaces) {
     double sinalRadicando = Math.signum(radicando);
 
     radicando *= sinalRadicando;
@@ -11,6 +21,18 @@ public class Calculadora {
       // Caso Invalido
       return Double.NaN;
     }
-    return Math.pow(radicando, 1.0/indice) * sinalRadicando;
+
+    double resultado = Math.pow(radicando, 1.0/indice) * sinalRadicando;
+
+    return Double.isNaN(resultado)? resultado : round(resultado, roundPlaces);
   }
+
+  private static double round(double value, int places) {
+    if (places < 0) throw new IllegalArgumentException();
+
+    BigDecimal bd = new BigDecimal(Double.toString(value));
+    bd = bd.setScale(places, RoundingMode.HALF_EVEN);
+    return bd.doubleValue();
+  }
+
 }
